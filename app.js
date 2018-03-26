7f3441ecda4ac0caf55cde3699ad021c74eb0f07
sergetoure09 <sergetoure09@gmail.com>
2018-03-26 22:18:20 +0100

    remove use case done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//import { stat } from "fs";
-
 var totalIncome = 0
 var totalExpense = 0
 var balance = 0
@@ -7,46 +5,46 @@ var expenseRate
 var localRate
 var expList
 
-
 var state = {
 
     transaction: [],
 
 }
-
-
-
 var transaction = function (props) {
     this.transacType = props.type,
         this.description = props.description,
         this.amount = props.amount
 
-
-
 }
-
 
 transaction.prototype = {
 
     add: function () {
-        // verify integrity
-        //console.log(this)
         state.transaction.unshift(this)
         check_push_item(state.transaction, push)
         console.log(state.transaction)
+        btnRemove = document.querySelectorAll('.remove')
+        btnRemove.forEach(el => {
+            el.onclick = function (event) {
+                console.log('you delete element')
+                state.transaction[(Number(event.target.dataset.key))].remove(Number(event.target.dataset.key))
 
-
+            }
+        })
 
     },
 
-    remove: function(key) {
-        //let key=state.transaction.indexOf(this)
-       // console.log(state.transaction)
-        state.transaction.splice(key,1)
-        //console.log(state.transaction)
+    remove: function (key) {
+        state.transaction.splice(key, 1)
         check_push_item(state.transaction, push)
+        btnRemove = document.querySelectorAll('.remove')
+        btnRemove.forEach(el => {
+            el.onclick = function (event) {
+                console.log('you delete element')
+                state.transaction[(Number(event.target.dataset.key))].remove(Number(event.target.dataset.key))
 
-
+            }
+        })
     }
 
 
@@ -55,10 +53,10 @@ var income = document.querySelector("#income")
 var expense = document.querySelector("#expense")
 
 var rate = document.querySelector("#rate")
-var localRate = document.querySelector(".localRate") //return NodeList
+var localRate = document.querySelector(".localRate") 
 
 var btnAdd = document.querySelector("#add")
-var btnRemove=[]
+
 
 var desc = document.querySelector('#desc')
 var amount = document.querySelector('#amount')
@@ -69,37 +67,24 @@ var expPart = document.querySelector('#expPart')
 
 var globalBalance = document.querySelector('#globalBalance')
 
-
-
-
-/*Add fucntionality
-
-2-check for type(exp or inc)
-3-push to the corresponding part(expense or income)
-
-*/
-
 btnAdd.addEventListener('click', function () {
 
     var data = {
         type: type.value,
         description: desc.value,
         amount: Number(amount.value),
-        //expRate:
+        
     }
 
     if (data.description != '' && data.amount != '') {
         var item = new transaction(data)
-        //console.log(item)
+        
 
         item.add()
-       // bindDel()
+        
     } else {
         alert("Please provide transaction information...")
     }
-
-
-
 
 })
 
@@ -114,10 +99,8 @@ var check_push_item = function (itemList, pushFn) {
     var template
     incPart.innerHTML = ''
     expPart.innerHTML = ''
-
-    itemList.forEach(function (item,key) {
-
-
+//ADD CASE WHERE THERE IS NOTHING IN THE LIST AND INIT INCOME AND GLOBAL BALANCE
+    itemList.forEach(function (item, key) {
 
 
         if (item.transacType === 'inc') {
@@ -127,7 +110,7 @@ var check_push_item = function (itemList, pushFn) {
 
             template = `<div class="budget__sticker u-budget-sticker u-budget-sticker-income">
                 <span class="budget__sticker__title u-title">${item.description}</span>
-                <span class="budget__sticker__del"> <i data-key=${key} class="material-icons remove">highlight_off</i></span>
+                <span class="budget__sticker__del"> <i id=inc_${key} data-key=${key} class="material-icons remove">highlight_off</i></span>
                 <span class="budget__sticker__inc u-inc u-display-none">+2%</span>
                 <span class="budget__sticker__amount u-margin-right-adjust">+ ${item.amount}</span>  
         </div>`
@@ -146,19 +129,18 @@ var check_push_item = function (itemList, pushFn) {
                 currency: "EUR"
             })
 
-
-
-
         } else if (item.transacType === 'exp') {
-            
-            var total = state.transaction.filter(el => el.transacType === 'inc').map(el => el.amount).reduce((acc, el) => acc + el)
-         
+            try {
+                var total = state.transaction.filter(el => el.transacType === 'inc').map(el => el.amount).reduce((acc, el) => acc + el)
+            } catch (error) {
+                alert(`Vous ne pouvez pas faire ca tchai`)
+            }
             console.log(totalIncome)
             //expList.push({description:item.description,amount:Number(item.amount)})
             template = `<div class="budget__sticker u-budget-sticker u-budget-sticker-expenses">
                 <span class="budget__sticker__title u-title">${item.description}</span>
-                <span  class="budget__sticker__del"> <i data-key=${key}  class="material-icons remove">highlight_off</i></span>
-                <span class="budget__sticker__inc u-inc">${((item.amount / total) * 100).toFixed(1)}%</span>
+                <span  class="budget__sticker__del"> <i id=exp_${key} data-key=${key} class="material-icons remove">highlight_off</i></span>
+                <span class="budget__sticker__inc u-inc">${total!=0?((item.amount / total) * 100).toFixed(1):0}%</span>
                <span class="budget__sticker__amount">- ${item.amount}</span>
               </div>`
 
@@ -174,7 +156,7 @@ var check_push_item = function (itemList, pushFn) {
             expense.textContent = totalExpense.toLocaleString("fr-FR", {
                 style: "currency",
                 currency: "EUR"
-            })    
+            })
 
         }
 
@@ -185,19 +167,10 @@ var check_push_item = function (itemList, pushFn) {
     init()
 }
 
-
-
-
 var push = function (template, htmlPart) {
     console.log("adding to dom...")
     htmlPart.innerHTML += template
-    btnRemove.push(document.querySelector(".remove"))
-    bindDel()
-
-
 }
-
-
 var incrementIncome = function (val) {
     totalIncome += val
 }
@@ -205,23 +178,8 @@ var incrementIncome = function (val) {
 var incrementExpense = function (val) {
     totalExpense += val
 }
-
-
 var init = function () {
     desc.value = ''
     amount.value = ''
-
 }
 
-//bindDel()
-
-   var bindDel=function(){
-    btnRemove.forEach(el=>{
-        el.onclick=function(){
-            console.log('you delete element')
-
-            state.transaction[(Number(el.dataset.key))].remove(Number(el.dataset.key))
-
-        }
-    })
-   }
